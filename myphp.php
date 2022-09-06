5ec158c7bfd96582e9e5abdc4fc740df0da2e36a
RXBSS <rxf.bss@gmail.com>
2022-09-06 15:23:51 +0200

    select data.

---
+++ myphp.php
@@ -0,0 +1,34 @@
+<?php
+
+$servername = "127.0.0.1";
+$username = "root@localhost";
+$password = "";
+$dbname = "rxf";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, vorname, nachname, rufnummer, wohnort, geschlecht FROM menschen";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo
+        "ID: " . $row["id"] .
+            " - NAME: " . $row["vorname"] . " " . $row["nachname"] .
+            " - NUMMER: " . $row["rufnummer"] .
+            " - WOHNSITZ" . $row["wohnort"] .
+            " - M/W: " . $row["geschlecht"] .
+            "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+
+
+?>
